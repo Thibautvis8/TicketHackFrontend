162e49ed4cb05bce8cf778566a42abc3e916afc8
Allan <christoph.allan77120@gmail.com>
2026-01-28 09:15:36 +0100

    frontend Allan

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ fetch("http://localhost:3000/trips").then(response => response.json()).then(allT
     console.log(allTrips);
     
 });
+
+fetch("http://localhost:3000/trips", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ departure: "Paris", arrival: "Lyon", date: "2026-01-27", price: 50 })
+});
+
+document.querySelector('#departureName').innerHTML += ``
+
+
+
+document.querySelector('#Search').addEventListener ('click', function() {
+
+})
